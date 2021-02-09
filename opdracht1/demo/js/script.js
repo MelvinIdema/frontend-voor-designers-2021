850087093c2d41c3c75c545cd16934689873de10
melvinidema <melvin_i13@hotmail.com>
2021-02-09 13:35:03 +0100

    Initial Commit

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -1,6 +1,10 @@
-/*
-  Wat doe je ook alweer in Javascript voor een micro-interactie?
-  1. Gebruik de querySelector om een element in je html te selecteren
-  2. Koppel een evenListener aan het element om een mouse-event te detecteren
-  3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
-*/
+const search = document.querySelector("#search");
+const colorFilter = document.querySelector(".filter-menu");
+
+search.addEventListener("focus", _ => {
+    colorFilter.classList.add("show");
+})
+
+search.addEventListener("blur", _ => {
+    colorFilter.classList.remove('show');
+})
